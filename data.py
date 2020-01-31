46494175e72efa81c03a3f2529f19dfc725b4408
Ulrike Gartler <ulrike.gartler@edu.fh-joanneum.at>
2020-01-31 18:22:38 +0100

    pushed all my files

--- data.py
+++ data.py
@@ -1,27 +1,34 @@
 import mysql.connector
 import mysql
 
+def importData(allContacts):
+	db = mysql.connector.connect(
+		host="localhost",
+		user ="root",
+		passwd="IwidLG47mBadS.",
+	)
 
-db = mysql.connector.connect(
-    host="localhost",
-    user ="root",
-    passwd="IwidLG47mBadS.",
-)
+	cursor = db.cursor()
+	
+	cursor.execute("create database if not exists birthdays") #else if exists delete it and create a new one
 
-cursor = db.cursor()
-cursor.execute("create database if not exists birthdays")
-
-
-persons = [
-			('Gerlinde', 'Saal', '9999-12-05', 'gerlinde.saal@gmx.at'), 
-			('Sanja', 'Hops', '1983-02-04', 'sanja83@yahoo.de')
-		   ]
-
-cursor.execute("use birthdays")
-cursor.execute("create table if not exists persons (firstName varchar(30) not null, lastName varchar (40), birthday date not null, email varchar (50) not null primary key)")
+	cursor.execute("use birthdays")
+	cursor.execute("create table if not exists persons (firstName varchar(30) not null, lastName varchar (40), birthday date not null, email varchar (50) not null primary key)")
 
 
-sql = "INSERT INTO persons (firstName, lastName, birthday, email) VALUES (%s, %s, %s, %s)"
+		
+	sql = "INSERT INTO persons (firstName, lastName, birthday, email) VALUES (%s, %s, %s, %s)"
 
-cursor.executemany(sql, persons)
-db.commit()
+	for x in allContacts:
+		cursor.execute(sql, (x.firstname, x.lastname, x.birthday, x.email))
+	
+	'''for x in persons:
+		firstName = x.firstname
+		lastname = x.lastname
+		birthday = x.birthday
+		email = x.email
+		'''
+	
+	
+		
+	db.commit()
