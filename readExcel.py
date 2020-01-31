bb6daade9ca9bc6ae13ec9852ca3c8e61d49a46c
Laura Groettrup <laura.groettrup@edu.fh-joanneum.at>
2020-01-31 16:45:23 +0100

    logic in a function so it is callable

--- readExcel.py
+++ readExcel.py
@@ -13,24 +13,24 @@ class Person:
         self.birthday = birthday
         self.email = email
 
-firstLine = True
-with open('contacts.csv') as csvfile:
-    readCSV = csv.reader(csvfile, delimiter=',')
-    for row in readCSV:
-        # Skip Header
-        if (firstLine):
-            firstLine = False
-        # Each entry of the contacts becomes a person
-        else:
-            # row[1]=firstname row[3]=lastname row[14]=birthday row[30]=email
-            date = row [14]
-            # Must have attributes: first name, email, birthday
-            if (row[1]!= ' ' or date!=' ' or row[30]!=' '):
-                if re.match(patternNoYear, date):
-                    date = re.sub(patternToReplace, '0000', date)
-                person = Person(row[1], row[3], date, row[30])
-                allContacts.append(person)
-                
+def readContacts():
+    firstLine = True
+    with open('contacts.csv') as csvfile:
+        readCSV = csv.reader(csvfile, delimiter=',')
+        for row in readCSV:
+            # Skip Header
+            if (firstLine):
+                firstLine = False
+            # Each entry of the contacts becomes a person
+            else:
+                # row[1]=firstname row[3]=lastname row[14]=birthday row[30]=email
+                date = row [14]
+                # Must have attributes: first name, email, birthday
+                if (row[1]!= ' ' or date!=' ' or row[30]!=' '):
+                    if re.match(patternNoYear, date):
+                        date = re.sub(patternToReplace, '0000', date)
+                    person = Person(row[1], row[3], date, row[30])
+                    allContacts.append(person)              
     # For testing
     for person in allContacts:
         print(person.firstname + ' ' + person.lastname + ' ' + person.birthday + ' ' + person.email)
